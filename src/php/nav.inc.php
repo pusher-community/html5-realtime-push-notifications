8868ecaa6bb3541987c4f94d663aa2ea04291519
Phil Leggetter <phil@leggetter.co.uk>
2011-12-09 15:36:00 +0000

    changing to a much simplified example

--- src/php/nav.inc.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" type="text/css" href="../lib/twitter-bootstrap/bootstrap.min.css" />
-
-<nav>
-  <a href="create.php">create</a> |
-  <a href="read.php">read</a> |
-  <a href="update.php">update</a> |
-  <a href="delete.php">delete</a> |
-  <a href="announce.php?message=Somebody has just navigated to the announce page">announce</a>     
-</nav>
---
+++ src/php/notify.php
@@ -0,0 +1,15 @@
+<?php
+require_once('config.php');
+require_once('../lib/squeeks-Pusher-PHP/lib/Pusher.php');
+
+$pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID);
+
+$message = sanitize( $_GET['message'] );
+$data = array('message' => $message);
+
+$pusher->trigger('my_notifications', 'notification', $data);
+
+function sanitize($data) {
+  return htmlspecialchars($data);
+}
+?>
