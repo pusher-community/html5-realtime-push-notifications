e81eaf97358d6f1bd8cb68f9a4da0f42180e7ea9
Phil Leggetter <phil@leggetter.co.uk>
2012-02-29 09:55:43 +0000

    adding the ability to specify some options

--- src/PusherNotifier.js
+++ src/PusherNotifier.js
@@ -1,10 +1,21 @@
-function PusherNotifier(channel) {
-  channel.bind('notification', this._handleNotification);
+function PusherNotifier(channel, options) {
+  options = options || {};
+  
+  this.settings = {
+    eventName: 'notifications',
+    title: 'Notification',
+    image: 'images/notify.png'
+  };
+  
+  $.extend(this.settings, options);
+  
+  var self = this;
+  channel.bind(this.settings.eventName, function(data){ self._handleNotification(data); });
 };
 PusherNotifier.prototype._handleNotification = function(data) {
   $.gritter.add({
-   title: 'Notification',
+   title: this.settings.title,
    text: data.message.replace(/\\/g, ''),
-   image: 'images/notify.png'
+   image: this.settings.image
   });
 };
